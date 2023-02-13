afca2e1c1f21986b0dfc5b8de6bf05dbb02d5a12
Brajesh Ahirwar <brajeshahirwar@gmai.com>
2023-02-13 21:05:19 +0530

    hi

--- test.py
+++ test.py
@@ -4,5 +4,4 @@ day, month, year = dates.split('-')
 day_name = datetime.date(int(year), int(month), int(day))
 print(day_name+datetime.timedelta(1))
 print(day_name.strftime("%A"))
-print(datetime.date(1912,12,13))
 
