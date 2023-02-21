0fe829d23c5f98e9b48f775466434d8826de9f4d
Brajesh Ahirwar <brajeshahirwar@gmai.com>
2023-02-21 23:24:18 +0530

    linked list file updated

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-dates=str(input('Enter the date(for example:23-02-2019):'))
-day, month, year = dates.split('-')
-day_name = datetime.date(int(year), int(month), int(day))
-print(day_name+datetime.timedelta(1))
-print(day_name.strftime("%A"))
-
